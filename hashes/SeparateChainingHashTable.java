30ba11a7598a584d4d000b14bca918a73de0be04
Yasir Mohamood <Yaska@Yasirs-MacBook-Pro.local>
2019-03-27 22:54:24 -0400

    created SeparateChaining class

---
+++ hashes/SeparateChainingHashTable.java
@@ -0,0 +1,40 @@
+public class SeparateChainingHashTable implements HashTable {
+
+	//Private fields & methods
+
+
+
+	//Public fields & methods
+
+	public SeparateChainingHashTable(){
+
+	}
+
+	void put(String key, String Valye){
+
+	}
+
+	String get(String key){
+
+	}
+
+	String remove(String key){
+
+	}
+
+	boolean containsKey(String key){
+
+	}
+
+	boolean containsValue(String value){
+
+	}
+
+	int size(){
+
+	}
+
+	int capacity(){
+		
+	}
+}
